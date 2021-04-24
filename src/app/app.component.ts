fb1bf204edcec051d37c6b9b3c680a21785933c8
jcizaurralde <jcizaurralde@gmail.com>
2021-04-24 14:20:37 -0300

    First commit, site construction begins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HolaMundo';
+  presentacion = "";
+  download = "";
+  textSearch= "";
+
+  constructor(){
+    setInterval(() => this.presentacion = "Hi, my name is Juan Cruz", 2000);
+    setInterval(() => this.download = "Download CV Juan Cruz Izaurralde", 3000);
+  }
+  downloadCV(){
+    this.download = "downloading";
+  }
 }
